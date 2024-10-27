1d15c0d28701b55f78096517f71c1dcbb5f61a70
MRMartini <michael.martini6455@gmail.com>
2024-10-26 22:20:28 -0500

    Added logic to seperate buttons into row divs. Altered js logic to create rows of buttons rather than just naked buttons;

--- logic.js
+++ logic.js
@@ -32,10 +32,19 @@ function operate(numOne, operator, numTwo) {
     }
 }
 
-//Populate button div
+//Populate row divs
 const container = document.querySelector(".buttons")
-for (let i = 0; i < 19; i++) {
-    const btn = document.createElement("button");
-    btn.classList.add("btn");
-    container.appendChild(btn);
+for (let i = 0; i < 5; i++) {
+    let btnCount = 4;
+    const row = document.createElement("div");
+    row.classList.add("row");
+    if (i === 4) {
+        btnCount = 3;
+    }
+    for (let j = 0; j < btnCount; j++) {
+        const btn = document.createElement("button");
+        btn.classList.add("btn");
+        row.appendChild(btn);
+    }
+    container.appendChild(row);
 }
